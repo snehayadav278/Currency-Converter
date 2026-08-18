c6cbc63d28e90714522d1bf4cdf5c23c8da3b807
snehayadav278 <snehay851@gmail.com>
2026-08-18 21:47:19 +0530

    Currency Converter project

---
+++ script.js
@@ -0,0 +1,82 @@
+const BASE_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+
+
+for (let select of dropdowns) {
+  for (let currCode in countryList) {
+    let newOption = document.createElement("option");
+    newOption.innerText = currCode;
+    newOption.value = currCode;
+
+    if (select.name === "From" && currCode === "USD") {
+      newOption.selected = "selected";
+    } else if (select.name === "To" && currCode === "INR") {
+      newOption.selected = "selected";
+    }
+    select.append(newOption);
+  }
+
+  select.addEventListener("change", (evt) => {
+    updateFlag(evt.target);
+  });
+}
+
+
+const updateExchangeRate = async () => {
+  let amount = document.querySelector(".amount input");
+  let amtVal = amount.value;
+  
+  if (amtVal === "" || amtVal < 1) {
+    amtVal = 1;
+    amount.value = "1";
+  }
+
+  let fromCode = fromCurr.value.toLowerCase();
+  let toCode = toCurr.value.toLowerCase();
+
+  const URL = `${BASE_URL}/${fromCode}.json`;
+
+  msg.innerText = "Getting exchange rate...";
+
+  try {
+    let response = await fetch(URL);
+    let data = await response.json();
+    
+    let rate = data[fromCode][toCode];
+
+    if (!rate) {
+      msg.innerText = "Rate unavailable.";
+      return;
+    }
+
+    let finalAmount = (amtVal * rate).toFixed(2);
+    msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+  } catch (error) {
+    msg.innerText = "Error loading exchange rate.";
+    console.error("GitHub API Fetch Error:", error);
+  }
+};
+
+
+const updateFlag = (element) => {
+  let currCode = element.value;
+  let countryCode = countryList[currCode]; 
+  let newSrc = `https://flagsapi.com{countryCode}/flat/64.png`; // ❌ yahan '.com' ke baad '/' missing hai
+
+  let img = element.parentElement.querySelector("img");
+  img.src = newSrc;
+};
+
+btn.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  updateExchangeRate();
+});
+
+window.addEventListener("load", () => {
+  updateExchangeRate();
+});
